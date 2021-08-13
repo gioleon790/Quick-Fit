d33aded3a9373df3453a2d3501c3d59ca3cb2bfc
Miguel Menjivar <miguel@miguels-mbp.lan>
2021-08-12 19:41:10 -0700

    minor changes

--- quick-fit/src/pages/ModelChoose.tsx
+++ quick-fit/src/pages/ModelChoose.tsx
@@ -1,44 +1,16 @@
-import React, { useState, useContext, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import {
   IonCard,
-  IonContent,
   IonHeader,
   IonPage,
-  IonSearchbar,
   IonTitle,
   IonToolbar,
-  IonCardHeader,
-  IonCardContent,
-  IonCardSubtitle,
-  IonCardTitle,
-  IonGrid,
   IonRow,
   IonCol,
-  IonTabBar,
-  IonTabButton,
   IonIcon,
-  IonLabel,
-  IonBadge,
-  IonTabs,
-  IonBackButton,
-  IonButtons,
-  IonItem,
-  IonList,
-  IonItemDivider,
-  IonInput,
-  IonButton,
+  IonButton
 } from '@ionic/react'
-import BrandComponent from '../components/BrandComponent'
 import {
-  calendar,
-  personCircle,
-  map,
-  informationCircle,
-  gridOutline,
-  cartOutline,
-  cameraOutline,
-  syncOutline,
-  swapHorizontalOutline,
   caretBackOutline,
   caretForwardOutline,
 } from 'ionicons/icons'
@@ -46,8 +18,8 @@ import UIContext from '../util/Context-API-login'
 import './ModelChoose.css'
 const ModelChoose: React.FC = () => {
   const { setShowTabs } = React.useContext(UIContext)
-  const [text, setText] = useState<string>()
-  const [number, setNumber] = useState<number>()
+  // const [text, setText] = useState<string>()
+  // const [number, setNumber] = useState<number>()
   const [switchModel, setSwitchModel] = useState<string>(
     'https://i.imgur.com/XhW77Wh.png'
   )
